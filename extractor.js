01450993adfb0b066d7f31202e85af6123348ab7
bhbghghbgb <hgbboyhuynhgiabao122003@yahoo.com>
2024-09-16 12:50:25 +0700

    some refactoring

--- extractor.js
+++ extractor.js
@@ -14,35 +14,34 @@ class VcbStatementExtractor {
     this.textLayer = textLayerElement;
     this.elements = [...textLayerElement.children];
     this.currentElementPos = -1;
+    this.currentElement = undefined;
     this.lastElementPos = this.elements.length - 1;
-    this.currentRowData = null;
+    // undefined: not yet parsed, null: not a text element, "": is text element but useless
+    // otherwise, stores the current parsed text
+    this.currentElementText = undefined;
+    this.currentRowData = undefined;
     this.completed = false;
   }
   extract() {}
-  // find and advance to the next row element position starting from next in current
-  toNextRow() {
+  // advance cursor
+  _toNextElement() {
     if (this.currentElementPos >= this.lastElementPos) {
-      console.info("Extractor completion.");
       this.completed = true;
       return false;
     }
+    this.currentElementText = undefined;
+    this.currentElement = this.elements[this.currentElementPos++];
+    return true;
   }
-  findNextRow() {
-    if (this.currentElementPos >= this.lastElementPos) {
-      console.info("Extractor completion.");
-      this.completed = true;
-      return false;
-    }
-    let i = this.currentElementPos;
-    do {
-      let element = this.elements[++i];
-    } while (i);
-    {
+  _eOfType() {
+    // TODO: determine type
+    if (this._eAsTnxDate()) {
+      return "tnxDate";
     }
   }
   // text is in date format DD/MM/YYYY
-  elementAsTnxDate(element) {
-    let elementText = this.elementAsText(element);
+  _eAsTnxDate() {
+    let elementText = this._eAsText();
     if (!elementText) {
       return null;
     }
@@ -59,8 +58,8 @@ class VcbStatementExtractor {
     return tnxDate.toDate();
   }
   // text is in format 1234.56789 (4digit.5digit)
-  elementAsDocno(element) {
-    let elementText = this.elementAsText(element);
+  _eAsDocNo() {
+    let elementText = this._eAsText();
     if (!elementText) {
       return null;
     }
@@ -75,12 +74,12 @@ class VcbStatementExtractor {
     }
     return parseInt(elementText.substr(0, 4) + elementText.substr(5));
   }
-  elementAsDebit(element) {
-    // unknown element position specifics.
+  _eAsDebit() {
+    // unknown element position specifics. unimplemented
   }
   // text is integer with 3 digit dot seperation
-  elementAsCredit(element) {
-    let elementText = this.elementAsText(element);
+  _eAsCredit() {
+    let elementText = this._eAsText();
     if (!elementText) {
       return null;
     }
@@ -88,8 +87,8 @@ class VcbStatementExtractor {
     let rightPosPercentage = this.calculateRightPosPercentage(element);
     if (
       // float tolerance
-      rightPosPercentage < 47.79 ||
-      rightPosPercentage > 47.81 ||
+      rightPosPercentage < 47.75 ||
+      rightPosPercentage > 47.85 ||
       element.style.fontSize !== "calc(var(--scale-factor)*8.00px)"
     ) {
       return null;
@@ -99,16 +98,58 @@ class VcbStatementExtractor {
     }
     return parseInt(elementText.replaceAll(".", ""));
   }
-  elementAsText(element) {
-    if (element.tagName !== "SPAN") {
+  // text is integer with 3 digit dot seperation
+  _eAsBalance() {
+    let elementText = this._eAsText();
+    if (!elementText) {
       return null;
     }
-    return element.textContent.trim() || null;
+    // the balance number is aligned to the right and we only have the left percentage
+    let rightPosPercentage = this.calculateRightPosPercentage(element);
+    if (
+      // float tolerance
+      rightPosPercentage < 63.49 ||
+      rightPosPercentage > 63.59 ||
+      element.style.fontSize !== "calc(var(--scale-factor)*8.00px)"
+    ) {
+      return null;
+    }
+    if (!/^\d+(\.\d{3})*$/.test(elementText)) {
+      return null;
+    }
+    return parseInt(elementText.replaceAll(".", ""));
+  }
+  _eAsTransactionsInDetail() {
+    let elementText = this._eAsText();
+    if (elementText == null) {
+      return null;
+    }
+    if (!/^[ -~]*$/.test(elementText)) {
+      return null;
+    }
+    return elementText;
+  }
+  // get current element text
+  // undefined: not yet parsed, null: not a text element, "": is text element but useless
+  // otherwise, stores the current parsed text
+  _eAsText() {
+    if (this.currentElementText !== undefined) {
+      return this.currentElementText;
+    }
+    if (element.tagName !== "SPAN") {
+      return (this.currentElementText = null);
+    }
+    // no need to trim spaces
+    return (this.currentElementText = element.textContent);
   }
   calculateRightPosPercentage(element) {
-    // left percentage
+    let left = element.style.left;
+    if (!left.endsWith("%")) {
+      return null;
+    }
     return (
-      parseFloat(element.style.left.substr(0, 5)) +
+      // left percentage
+      parseFloat(left.slice(0, -1)) +
       // width of element
       (element.getBoundingClientRect().width /
         // width of parent
@@ -117,7 +158,7 @@ class VcbStatementExtractor {
         100
     );
   }
-  elementIsFiller(element) {
+  _eIsFiller(element) {
     return element.tagName === "BR";
   }
 }
